b3b276410b21cb136981db7d79d36e94253c7d41
Charlotte <49044530+csdvrx@users.noreply.github.com>
2023-01-23 03:54:57 -0600

    Package downloader, to manually add to src.xs
    It would be better to use the Makefile, but it can be handy

---
+++ download_package.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use JSON::PP qw(decode_json);
+use LWP::UserAgent;
+use Data::Dumper qw(Dumper);
+
+my $module = shift @ARGV or die "Need a package.\nUsage:\n\t$0 Package::Name ~/Downloads";
+my $dirsave= shift @ARGV or die "Need a directory to save it.\nUsage:\n\t$0 Package::Name ~/Downloads";
+my $ua  = LWP::UserAgent->new;
+my $url = "https://fastapi.metacpan.org/v1/download_url/".$module;
+my $res = $ua->get($url);
+if(! $res->is_success) {
+    print Dumper($res);
+    die "Failed to find package";
+}
+my $jsonresponse = decode_json( $res->decoded_content );
+exists $jsonresponse->{download_url} or die "Failed to find package";
+$jsonresponse->{download_url} =~ /\/([a-zA-Z\-]+)\-[vV]?[\d\.]*((?:\.[a-zA-Z]+)+)$/ or die "Unable to parse out filename";
+my $finalname = "$dirsave/$1$2";
+my $tres = $ua->get($jsonresponse->{download_url});
+$tres->is_success or die "Failed to download package";
+open(my $fh, '>', $finalname) or die "Failed save package to disk";
+print $fh $tres->decoded_content;
+close($fh);
